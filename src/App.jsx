edf5173c873b2cea54c34cb1b69e2d10f6d85a1a
Kaung Htet San <kghtetsan26@gmail.com>
2025-01-15 13:39:25 +0630

    add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -20,10 +20,6 @@ export const App = () => {
 
   return (
     <div className="overflow-x-auto">
-      <button
-  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-  Button
-</button>
         <table className="table table-zebra">
           <thead>
             <tr>
