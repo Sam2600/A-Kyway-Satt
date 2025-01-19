7cb3263d2a6956348085c08187ce101e54098bbd
Kaung Htet San <kghtetsan26@gmail.com>
2025-01-19 18:11:42 +0630

    modify to fetch debt list query only with current user id

--- src/pages/debts/DebtList.jsx
+++ src/pages/debts/DebtList.jsx
@@ -34,7 +34,7 @@ const TABS = [
   },
 ];
 
-const TABLE_HEAD = ["No", "Owner", "Taker", "Item", "Amount", "Status", "Action"]; 
+const TABLE_HEAD= ["No", "Owner", "Taker", "Item", "Amount", "Status", "Action"]; 
 
 export const DebtList = () => {
 
@@ -42,6 +42,19 @@ export const DebtList = () => {
   const [debtList, setDebtList] = useState([]);
 
   const getDebtList = async () => {
+
+    /**
+     * @description get a user id first
+     */
+    const { id } = await supabase.auth
+      .getSession()
+      .then((resp) => resp?.data)
+      .then((data) => data?.session)
+      .then((session) => session?.user);
+    
+    /**
+     * @description then get a query where with user id
+     */
     const { data } = await supabase.from("debts").select(`
       id,
       from:pay_from_user_id(name),
@@ -49,7 +62,7 @@ export const DebtList = () => {
       item:item_id(name),
       amount,
       status
-    `);
+    `).eq("pay_to_user_id", id);
     setDebtList(data);
   };
 
@@ -64,136 +77,137 @@ export const DebtList = () => {
   }, [search]);
 
   return (
-      <Card className="h-full w-full border border-gray-400 rounded-lg">
-        <CardHeader floated={false} shadow={false} className="rounded-none">
-          <div className="mb-8 flex items-center justify-between gap-8">
-            <div>
-              <Typography variant="h5" color="blue-gray">
-                Debt List
-              </Typography>
-              <Typography color="gray" className="mt-1 font-normal">
-                See information about debts of me
-              </Typography>
-            </div>
-            <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
-              <Button variant="outlined" size="sm">
-                view all
-              </Button>
-              <Button size="sm" >
-                <NavLink className="flex items-center gap-2" to={"/add-debt"}>
-                  <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add Debt  
-                </NavLink>
-              </Button>
-            </div>
+    <Card className="h-full w-full border border-gray-400 rounded-lg">
+      <CardHeader floated={false} shadow={false} className="rounded-none">
+        <div className="mb-8 flex items-center justify-between gap-8">
+          <div>
+            <Typography variant="h5" color="blue-gray">
+              Debt List
+            </Typography>
+            <Typography color="gray" className="mt-1 font-normal">
+              See information about debts of me
+            </Typography>
           </div>
-          <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
-            <Tabs value="all" className="w-full -z-0 md:w-max">
-              <TabsHeader>
-                {TABS.map(({ label, value }) => (
-                  <Tab key={value} value={value}>
-                    &nbsp;&nbsp;{label}&nbsp;&nbsp;
-                  </Tab>
-                ))}
-              </TabsHeader>
-            </Tabs>
-            <div className="w-full md:w-72">
-              <Input
-                label="Search"
-                icon={<MagnifyingGlassIcon className="h-5 w-5" />}
-              />
-            </div>
+          <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+            <Button variant="outlined" size="sm">
+              view all
+            </Button>
+            <NavLink className="flex items-center gap-2" to={"/add-debt"}>
+              <Button size="sm" className="flex justify-between gap-x-2">
+                <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add Debt
+              </Button>
+            </NavLink>
           </div>
-        </CardHeader>
-        <CardBody className="overflow-scroll px-0">
-          <table className="mt-4 w-full min-w-max table-auto text-left">
-            <thead>
-              <tr>
-                {TABLE_HEAD.map((head) => (
-                  <th
-                    key={head}
-                    className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
-                  >
-                    <Typography
-                      variant="small"
-                      color="blue-gray"
-                      className="font-normal leading-none opacity-70"
-                    >
-                      {head}
-                    </Typography>
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {debtList.map((debt, index) => {
-                  const isLast = index === debtList.length - 1;
-                  const classes = isLast
-                    ? "p-4"
-                    : "p-4 border-b border-blue-gray-50";
+        </div>
+        <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+          <Tabs value="all" className="w-full -z-0 md:w-max">
+            <TabsHeader>
+              {TABS.map(({ label, value }) => (
+                <Tab key={value} value={value}>
+                  &nbsp;&nbsp;{label}&nbsp;&nbsp;
+                </Tab>
+              ))}
+            </TabsHeader>
+          </Tabs>
+          <div className="w-full md:w-72">
+            <Input
+              label="Search"
+              icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+            />
+          </div>
+        </div>
+      </CardHeader>
 
-                  return (
-                    <tr key={debt?.id}>
-                      <td className={classes}>
-                        <div className="flex items-center gap-3">
-                          {/* <Avatar src={"#"} alt={"#"} size="sm" /> */}
-                          <div className="flex flex-col">
-                            <Typography
-                              variant="small"
-                              color="blue-gray"
-                              className="font-normal"
-                            >
-                              {index + 1}
-                            </Typography>
-                            {/* <Typography
+      <CardBody className="px-0"> {/* overflow-scroll */}
+        <table className="mt-4 w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              {TABLE_HEAD.map((head) => (
+                <th
+                  key={head}
+                  className="border-y border-blue-gray-100 bg-blue-gray-50/50 p-4"
+                >
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal leading-none opacity-70"
+                  >
+                    {head}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {debtList?.map((debt, index) => {
+              const isLast = index === debtList.length - 1;
+              const classes = isLast
+                ? "p-4"
+                : "p-4 border-b border-blue-gray-50";
+
+              return (
+                <tr key={debt?.id}>
+                  <td className={classes}>
+                    <div className="flex items-center gap-3">
+                      {/* <Avatar src={"#"} alt={"#"} size="sm" /> */}
+                      <div className="flex flex-col">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {index + 1}
+                        </Typography>
+                        {/* <Typography
                               variant="small"
                               color="blue-gray"
                               className="font-normal opacity-70"
                             > */}
-                              {/* {email} */}
-                            {/* </Typography> */}
-                          </div>
-                        </div>
-                      </td>
-                      <td className={classes}>
-                        <div className="flex items-center gap-3">
-                          {/* <Avatar src={"#"} alt={"#"} size="sm" /> */}
-                          <div className="flex flex-col">
-                            <Typography
-                              variant="small"
-                              color="blue-gray"
-                              className="font-normal"
-                            >
-                              {debt?.from?.name}
-                            </Typography>
-                            {/* <Typography
+                        {/* {email} */}
+                        {/* </Typography> */}
+                      </div>
+                    </div>
+                  </td>
+                  <td className={classes}>
+                    <div className="flex items-center gap-3">
+                      {/* <Avatar src={"#"} alt={"#"} size="sm" /> */}
+                      <div className="flex flex-col">
+                        <Typography
+                          variant="small"
+                          color="blue-gray"
+                          className="font-normal"
+                        >
+                          {debt?.to?.name}
+                        </Typography>
+                        {/* <Typography
                               variant="small"
                               color="blue-gray"
                               className="font-normal opacity-70"
                             > */}
-                              {/* {email} */}
-                            {/* </Typography> */}
-                          </div>
-                        </div>
-                      </td>
-                      <td className={classes}>
-                        <div className="flex flex-col">
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {debt?.to?.name}
-                          </Typography>
-                          {/* <Typography
+                        {/* {email} */}
+                        {/* </Typography> */}
+                      </div>
+                    </div>
+                  </td>
+                  <td className={classes}>
+                    <div className="flex flex-col">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {debt?.from?.name}
+                      </Typography>
+                      {/* <Typography
                             variant="small"
                             color="blue-gray"
                             className="font-normal opacity-70"
                           > */}
-                            {/* {org} */}
-                          {/* </Typography> */}
-                        </div>
-                      </td>
-                      {/* <td className={classes}>
+                      {/* {org} */}
+                      {/* </Typography> */}
+                    </div>
+                  </td>
+                  {/* <td className={classes}>
                         <div className="w-max">
                           <Chip
                             variant="ghost"
@@ -203,70 +217,70 @@ export const DebtList = () => {
                           />
                         </div>
                       </td> */}
-                      <td className={classes}>
-                        <div className="flex flex-col">
-                          <Typography
-                            variant="small"
-                            color="blue-gray"
-                            className="font-normal"
-                          >
-                            {debt?.item?.name}
-                          </Typography>
-                          {/* <Typography
+                  <td className={classes}>
+                    <div className="flex flex-col">
+                      <Typography
+                        variant="small"
+                        color="blue-gray"
+                        className="font-normal"
+                      >
+                        {debt?.item?.name}
+                      </Typography>
+                      {/* <Typography
                             variant="small"
                             color="blue-gray"
                             className="font-normal opacity-70"
                           > */}
-                            {/* {org} */}
-                          {/* </Typography> */}
-                        </div>
-                      </td>
-                      <td className={classes}>
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {debt?.amount}
-                        </Typography>
-                      </td>
-                      <td className={classes}>
-                        <div className="w-max">
-                          <Chip
-                            variant="ghost"
-                            size="sm"
-                            value={debt?.status ? "done" : "Not Yet"}
-                            color={debt?.status ? "green" : "red"}
-                          />
-                        </div>
-                      </td>
-                      <td className={classes}>
-                        <Tooltip content="Edit User">
-                          <IconButton variant="text">
-                            <PencilIcon className="h-4 w-4" />
-                          </IconButton>
-                        </Tooltip>
-                      </td>
-                    </tr>
-                  );
-                },
-              )}
-            </tbody>
-          </table>
-        </CardBody>
-        <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
-          <Typography variant="small" color="blue-gray" className="font-normal">
-            Page 1 of 10
-          </Typography>
-          <div className="flex gap-2">
-            <Button variant="outlined" size="sm">
-              Previous
-            </Button>
-            <Button variant="outlined" size="sm">
-              Next
-            </Button>
-          </div>
-        </CardFooter>
-      </Card>
+                      {/* {org} */}
+                      {/* </Typography> */}
+                    </div>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {debt?.amount}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <div className="w-max">
+                      <Chip
+                        variant="ghost"
+                        size="sm"
+                        value={debt?.status ? "done" : "Not Yet"}
+                        color={debt?.status ? "green" : "red"}
+                      />
+                    </div>
+                  </td>
+                  <td className={classes}>
+                    <Tooltip content="Edit User">
+                      <IconButton variant="text">
+                        <PencilIcon className="h-4 w-4" />
+                      </IconButton>
+                    </Tooltip>
+                  </td>
+                </tr>
+              );
+            },
+            )}
+          </tbody>
+        </table>
+      </CardBody>
+      <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4">
+        <Typography variant="small" color="blue-gray" className="font-normal">
+          Page 1 of 10
+        </Typography>
+        <div className="flex gap-2">
+          <Button variant="outlined" size="sm">
+            Previous
+          </Button>
+          <Button variant="outlined" size="sm">
+            Next
+          </Button>
+        </div>
+      </CardFooter>
+    </Card>
   );
 }
