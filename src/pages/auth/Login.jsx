2fc918878cbdd1e1dc555ff15ea13ec5036055ef
Kaung Htet San <kghtetsan26@gmail.com>
2025-01-19 18:08:41 +0630

    add and modify error message show process + confirm password + eye icon to toggle password show/hide

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,117 +1,187 @@
 import React, { useState } from "react";
-import { supabase } from "../../database/SupabaseClient"
+import { supabase } from "../../database/SupabaseClient";
 import {
   Button,
   Card,
   Input,
   Typography,
+  Spinner,
 } from "@material-tailwind/react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { storeAuthSession } from "../../states/features/auth/authSlice";
+import { scrollToTop } from "../../utils/helper_functions/helper";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/16/solid";
 
 export const Login = () => {
+  //
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => setPasswordShown((cur) => !cur);
+  const [serverError, setserverError] = useState("");
 
   // UseForm hook
-  const { register, formState, handleSubmit, reset } = useForm();
+  const { register, formState, handleSubmit } = useForm();
 
   // Useful Form states
   const { errors, isSubmitting } = formState;
 
-  const [success, setSuccess] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [serverError, setserverError] = useState("");
+  const onError = (errors, e) => {
+    console.log(e);
+    console.log(errors);
+    scrollToTop();
+  };
 
   const onSubmit = async (data) => {
     //
-    setLoading(true);
-
     let { email, password } = data;
-  
-    let response = await supabase.auth.signInWithPassword({ email, password });
 
-    console.log(response);
-  };
-
-  const onError = (errors, e) => {
-    setSuccess(false);
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: "smooth",
-    });
+    await supabase.auth.signInWithPassword({ email, password })
+      .then(response => response?.data)
+      .then(data => {
+        dispatch(storeAuthSession(data?.user))
+        navigate("/");
+      })
+      .catch(error => {
+          setserverError(error?.message);
+          scrollToTop();
+        });
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <Card className="w-full max-w-sm p-6">
-        <Typography variant="h4" className="text-center mb-4">
-          Login
-        </Typography>
-        <form className="space-y-4" method="POST" onSubmit={handleSubmit(onSubmit, onError)}>
-          <Input
-            label="Email"
-            size="lg"
-            type="email"
-            {...register("email", {
-              required: {
-                value: true,
-                message: "Email is required",
-              },
-
-              pattern: {
-                value:
-                  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-                message: "Please enter a valid email format",
-              },
-
-              // Validate for multiple conditions
-              validate: {
-                notAdmin: (value) =>
-                  value !== "admin@gmail.com" ||
-                  "Please try with different email",
-                badDomain: (value) =>
-                  !value.endsWith("customMail.com") ||
-                  "Bad domain for email",
-                noScriptTags: (value) =>
-                  !/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi.test(
-                    value
-                  ) || "Script tags are not allowed",
-              },
-            })}
-          />
-          <Input
-            label="Password"
-            size="lg"
-            type="password"
-            {...register("password", {
-              required: {
-                value: true,
-                message: "Password is required",
-              },
-
-              // Validate for multiple conditions
-              validate: {
-                tooShort: (value) =>
-                  value.length > 7 ||
-                  "Password too short",
-              },
-            })}
-          />
-          <Button
-            size="lg"
-            fullWidth
-            type="submit"
-          >
+      <Card className="w-full max-w-md p-6">
+        <div className="flex flex-col gap-2 text-center mb-14">
+          <Typography variant="h3" color="blue-gray">
             Login
-          </Button>
-        </form>
-        <Typography
-          variant="small"
-          className="text-center mt-4 text-gray-600"
+          </Typography>
+          <Typography className="text-gray-600 font-normal text-[18px]">
+            Enter your email and password to sign in
+          </Typography>
+        </div>
+        {serverError && (
+          <p className="text-white bg-red-500 p-3 -mt-5 mb-9 rounded-md">
+            {serverError}
+          </p>
+        )}
+        <form
+          className="space-y-4"
+          method="POST"
+          onSubmit={handleSubmit(onSubmit, onError)}
         >
-          Not a member? {" "} <NavLink className="hover:underline text-blue-500" to={"/register"}>{" Sign up "}</NavLink>
-        </Typography>
-      </Card> 
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography
+              variant="h6"
+              color="blue-gray"
+              className="-mb-4 flex justify-between"
+            >
+              Your Email
+              <p className="block font-semibold mb-[2px] text-red-500">
+                {errors?.email?.message}
+              </p>
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email is required",
+                },
+
+                pattern: {
+                  value:
+                    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: "Please enter a valid email format",
+                },
+
+                // Validate for multiple conditions
+                validate: {
+                  notAdmin: (value) =>
+                    value !== "admin@gmail.com" ||
+                    "Please try with different email",
+                  badDomain: (value) =>
+                    !value.endsWith("customMail.com") || "Bad domain for email",
+                  noScriptTags: (value) =>
+                    !/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi.test(
+                      value
+                    ) || "Script tags are not allowed",
+                },
+              })}
+            />
+            <Typography
+              variant="h6"
+              color="blue-gray"
+              className="-mb-4 flex justify-between"
+            >
+              Password
+              <p className="block font-semibold mb-[2px] text-red-500">
+                {errors?.password?.message}
+              </p>
+            </Typography>
+            <Input
+              type={passwordShown ? "text" : "password"}
+              size="lg"
+              placeholder="********"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              icon={
+                <i onClick={togglePasswordVisiblity}>
+                  {passwordShown ? (
+                    <EyeIcon className="h-5 w-5" />
+                  ) : (
+                    <EyeSlashIcon className="h-5 w-5" />
+                  )}
+                </i>
+              }
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "Password is required",
+                },
+
+                // Validate for multiple conditions
+                validate: {
+                  tooShort: (value) =>
+                    value.length > 7 || "Password is too short",
+                },
+              })}
+            />
+            <Button
+              disabled={isSubmitting}
+              className="mt-6 flex justify-center items-center"
+              size="lg"
+              fullWidth
+              type="submit"
+            >
+              {isSubmitting ? <Spinner /> : "LOGIN"}
+            </Button>
+            <Typography
+              variant="small"
+              className="text-center mt-4 text-gray-600"
+            >
+              Not a member?{" "}
+              {!isSubmitting && (
+                <NavLink
+                  className="hover:underline text-blue-500"
+                  to={"/register"}
+                >
+                  {"Sign up"}
+                </NavLink>
+              )}
+            </Typography>
+          </div>
+        </form>
+      </Card>
     </div>
   );
-}
+};
--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,85 +1,259 @@
+import React, { useState } from "react";
+import { supabase } from "../../database/SupabaseClient";
 import {
-    Card,
-    Input,
-    Checkbox,
-    Button,
-    Typography,
+  Card,
+  Input,
+  Button,
+  Typography,
+  Spinner,
 } from "@material-tailwind/react";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { storeAuthSession } from "../../states/features/auth/authSlice";
+import { scrollToTop } from "../../utils/helper_functions/helper";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/16/solid";
 
 export const Register = () => {
-    return (
-        <Card color="transparent" shadow={false}>
-            <Typography variant="h4" color="blue-gray">
-            Sign Up
+  //
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [serverError, setserverError] = useState("");
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePasswordVisiblity = () => setPasswordShown((cur) => !cur);
+
+  // UseForm hook
+  const { register, formState, handleSubmit, watch } = useForm();
+
+  // Useful Form states
+  const { errors, isSubmitting } = formState;
+
+  const onError = (errors, e) => {
+    console.log(e);
+    console.log(errors);
+    scrollToTop();
+  };
+
+  const onSubmit = async (data) => {
+    //
+
+    let { name, email, password } = data;
+
+    let options = {
+      data: {
+        name: name,
+      },
+    };
+
+    let response = await supabase.auth.signUp({
+      email,
+      password,
+      options,
+    });
+
+    if (response?.error) {
+      setserverError(response?.error?.message);
+      scrollToTop();
+    } else {
+      dispatch(storeAuthSession(response?.data?.session));
+      navigate("/login");
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card className="w-full max-w-md p-6">
+        <div className="text-center">
+          <Typography variant="h3" color="blue-gray" className="mb-2">
+            Register
+          </Typography>
+          <Typography className="mb-8 text-gray-600 font-normal text-[18px]">
+            Have fun taking debt note {":)"}
+          </Typography>
+        </div>
+
+        <form
+          className="space-y-4"
+          method="POST"
+          onSubmit={handleSubmit(onSubmit, onError)}
+        >
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography
+              variant="h6"
+              color="blue-gray"
+              className="-mb-4 flex justify-between"
+            >
+              Your Name
+              <p className="block font-semibold mb-[2px] text-red-500">
+                {errors?.name?.message}
+              </p>
             </Typography>
-            <Typography color="gray" className="mt-1 font-normal">
-            Nice to meet you! Enter your details to register.
-            </Typography>
-            <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-            <div className="mb-1 flex flex-col gap-6">
-                <Typography variant="h6" color="blue-gray" className="-mb-3">
-                Your Name
-                </Typography>
-                <Input
-                size="lg"
-                placeholder="name@mail.com"
-                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                labelProps={{
-                    className: "before:content-none after:content-none",
-                }}
-                />
-                <Typography variant="h6" color="blue-gray" className="-mb-3">
-                Your Email
-                </Typography>
-                <Input
-                size="lg"
-                placeholder="name@mail.com"
-                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                labelProps={{
-                    className: "before:content-none after:content-none",
-                }}
-                />
-                <Typography variant="h6" color="blue-gray" className="-mb-3">
-                Password
-                </Typography>
-                <Input
-                type="password"
-                size="lg"
-                placeholder="********"
-                className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                labelProps={{
-                    className: "before:content-none after:content-none",
-                }}
-                />
-            </div>
-            <Checkbox
-                label={
-                <Typography
-                    variant="small"
-                    color="gray"
-                    className="flex items-center font-normal"
-                >
-                    I agree the
-                    <a
-                    href="#"
-                    className="font-medium transition-colors hover:text-gray-900"
-                    >
-                    &nbsp;Terms and Conditions
-                    </a>
-                </Typography>
-                }
-                containerProps={{ className: "-ml-2.5" }}
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              {...register("name", {
+                required: {
+                  value: true,
+                  message: "Name is required",
+                },
+
+                pattern: {
+                  value: /^[A-Za-z\s]+$/,
+                  message: "Only alphabetic characters are allowed",
+                },
+              })}
             />
-            <Button className="mt-6" fullWidth>
-                sign up
-            </Button>
-            <Typography color="gray" className="mt-4 text-center font-normal">
-                Already have an account?{" "}
-                <a href="#" className="font-medium text-gray-900">
-                Sign In
-                </a>
+            <Typography
+              variant="h6"
+              color="blue-gray"
+              className="-mb-4 flex justify-between"
+            >
+              Your Email
+              <p className="block font-semibold mb-[2px] text-red-500">
+                {errors?.email?.message}
+              </p>
             </Typography>
-            </form>
-        </Card>
-    );
-}
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Email is required",
+                },
+
+                pattern: {
+                  value:
+                    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: "Please enter a valid email format",
+                },
+
+                // Validate for multiple conditions
+                validate: {
+                  notAdmin: (value) =>
+                    value !== "admin@gmail.com" ||
+                    "Please try with different email",
+                  badDomain: (value) =>
+                    !value.endsWith("customMail.com") || "Bad domain for email",
+                  noScriptTags: (value) =>
+                    !/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi.test(
+                      value
+                    ) || "Script tags are not allowed",
+                },
+              })}
+            />
+            <Typography
+              variant="h6"
+              color="blue-gray"
+              className="-mb-4 flex justify-between"
+            >
+              Password
+              <p className="block font-semibold mb-[2px] text-red-500">
+                {errors?.password?.message}
+              </p>
+            </Typography>
+            <Input
+              type={passwordShown ? "text" : "password"}
+              size="lg"
+              placeholder="********"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              icon={
+                <i onClick={togglePasswordVisiblity}>
+                  {passwordShown ? (
+                    <EyeIcon className="h-5 w-5" />
+                  ) : (
+                    <EyeSlashIcon className="h-5 w-5" />
+                  )}
+                </i>
+              }
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "Password is required",
+                },
+
+                // Validate for multiple conditions
+                validate: {
+                  tooShort: (value) => value.length > 7 || "Password too short",
+                },
+              })}
+            />
+            <Typography
+              variant="h6"
+              color="blue-gray"
+              className="-mb-4 flex justify-between"
+            >
+              <span className="flex"> Confirm Password </span>
+              <p className="block font-semibold mb-[2px] text-red-500">
+                {errors?.confirm_password?.message}
+              </p>
+            </Typography>
+            <Input
+              type={passwordShown ? "text" : "password"}
+              size="lg"
+              placeholder="********"
+              className="!border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+              icon={
+                <i onClick={togglePasswordVisiblity}>
+                  {passwordShown ? (
+                    <EyeIcon className="h-5 w-5" />
+                  ) : (
+                    <EyeSlashIcon className="h-5 w-5" />
+                  )}
+                </i>
+              }
+              {...register("confirm_password", {
+                required: {
+                  value: true,
+                  message: "Confirm password is required",
+                },
+                validate: {
+                  matches: (value) =>
+                    value === watch("password") || "Passwords do not match",
+                },
+              })}
+            />
+            <Button
+              disabled={isSubmitting}
+              className="mt-6 flex justify-center items-center"
+              size="lg"
+              fullWidth
+              type="submit"
+            >
+              {isSubmitting ? <Spinner /> : "REGISTER"}
+            </Button>
+            <Typography
+              variant="small"
+              className="text-center mt-4 text-gray-600"
+            >
+              Alredy created an account?{" "}
+              {!isSubmitting && (
+                <NavLink
+                  className="hover:underline text-blue-500"
+                  to={"/login"}
+                >
+                  {" Login "}
+                </NavLink>
+              )}
+            </Typography>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
+};
