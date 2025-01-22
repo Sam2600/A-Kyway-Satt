e2cf896446ca8f08ed6dad592a2ba85229923700
Kaung Htet San <kghtetsan26@gmail.com>
2025-01-22 17:02:24 +0630

    add model form for item registeration and implemnt validaitons

--- src/pages/debts/AddDebt.jsx
+++ src/pages/debts/AddDebt.jsx
@@ -4,11 +4,14 @@ import {
    Button,
    Typography,
    Spinner,
+   Dialog,
+   CardBody,
+   CardFooter,
 } from "@material-tailwind/react";
 import { useEffect, useState } from "react";
 import { scrollToTop } from "../../utils/helper_functions/helper";
 import { supabase } from "../../database/SupabaseClient";
-import { useForm, Controller } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 
 export const AddDebt = () => {
    //
@@ -63,10 +66,23 @@ export const AddDebt = () => {
 
 
    const [success, setSuccess] = useState("");
+   const [modelSuccess, setModelSuccess] = useState("");
+
    const [serverError, setserverError] = useState("");
+   const [modelServerError, setModelServerError] = useState("");
+   
+   const [open, setOpen] = useState(false);
+   const handleAddItem = () => setOpen((cur) => !cur);
 
    // UseForm hook
    const { register, formState, handleSubmit, reset } = useForm();
+   
+   // UseForm hook for second model box form
+   const {
+      register: register2,
+      formState: { errors: errors2 },
+      handleSubmit: handleSubmit2,
+   } = useForm();
 
    // Useful Form states
    const { errors, isSubmitting } = formState;
@@ -88,12 +104,94 @@ export const AddDebt = () => {
 
       if (error?.message && error?.code) {
          console.log(error);
+         setModelServerError("");
          setserverError(error?.message);
       } else {
+         setModelSuccess("");
          setSuccess("Debt is created successfully");
          reset();
       }
    };
+
+   const onSubmit2 = async (data) => {
+      
+      let { error } = await supabase.from("items").insert(data);
+
+      if (error?.message && error?.code) {
+         console.log(error);
+         setserverError("");
+         setModelServerError(error?.message);
+         handleAddItem();
+      } else {
+         setserverError("");
+         setModelSuccess("Item is created successfully");
+         reset();
+         handleAddItem();
+      }
+   }
+
+   const onError2 = (errors2, e) => {
+      console.log(e);
+      console.log(errors2);
+   };
+
+   const addItemModel = (
+      <Dialog
+         size="xs"
+         open={open}
+         handler={handleAddItem}
+         className="bg-transparent shadow-none"
+      >
+         <form key={2} method="POST" onSubmit={handleSubmit2(onSubmit2, onError2)} className="w-full max-w-[24rem] mx-auto">
+            <Card className="mx-auto w-full max-w-[24rem]">
+               <CardBody className="flex flex-col gap-4">
+                  <Typography className="text-center mb-2" variant="h4" color="blue-gray">
+                     Add Item
+                  </Typography>
+                  <Typography variant="h6" color="blue-gray" className="-mb-3 flex justify-between">
+                     Item *
+                     <p className="block font-semibold mb-[2px] text-red-500">
+                        {errors2?.name?.message}
+                     </p>
+                  </Typography>
+                  <Input size="lg"
+                     placeholder="Item Name"
+                     className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                     labelProps={{
+                        className: "before:content-none after:content-none",
+                     }}
+                     {
+                        ...register2("name", {
+                           required: {
+                              value: true,
+                              message: "Item name is required",
+                           }
+                        })
+                     }
+                  />
+                  <Typography className="-mb-2" variant="h6">
+                     Description
+                  </Typography>
+                  <Input size="lg"
+                     placeholder="Description"
+                     className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+                     labelProps={{
+                        className: "before:content-none after:content-none",
+                     }}
+                     {
+                        ...register2("description")
+                     }
+                  />
+               </CardBody>
+               <CardFooter className="pt-0">
+                  <Button variant="gradient" type="submit" fullWidth>
+                     Add Item
+                  </Button>
+               </CardFooter>
+            </Card>
+         </form>
+      </Dialog>
+   );
 
    return (
       <Card className="items-center" color="transparent" shadow={false}>
@@ -108,19 +206,30 @@ export const AddDebt = () => {
                {serverError}
             </p>
          )}
+         {modelServerError && (
+            <p className="text-white bg-red-500 p-3 mt-6 rounded-md">
+               {modelServerError}
+            </p>
+         )}
          {success && (
             <p className="text-white bg-green-500 p-3 mt-6 rounded-md">
                {success}
             </p>
          )}
+         {modelSuccess && (
+            <p className="text-white bg-green-500 p-3 mt-6 rounded-md">
+               {modelSuccess}
+            </p>
+         )}
          <form
+         key={1}
          method="POST"
          onSubmit={handleSubmit(onSubmit, onError)}
          className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96"
          >
          <div className="mb-8 flex flex-col gap-6">
             <Typography variant="h6" color="blue-gray" className="-mb-3 flex justify-between">
-               Taker Name
+               Taker Name *
                <p className="block font-semibold mb-[2px] text-red-500">
                   {errors?.pay_from_user_id?.message}
                </p>
@@ -151,34 +260,37 @@ export const AddDebt = () => {
                })}
             </select>
             <Typography variant="h6" color="blue-gray" className="-mb-3 flex justify-between">
-               Item
+               Item *
                <p className="block font-semibold mb-[2px] text-red-500">
                   {errors?.item_id?.message}
                </p>
             </Typography>
-            <select
-               className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 disabled:cursor-not-allowed transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent placeholder:opacity-0 focus:placeholder:opacity-100 text-sm px-3 py-3 rounded-md border-blue-gray-200 focus:border-gray-900 !border-t-blue-gray-200 focus:!border-t-gray-900"
-               {...register("item_id", {
-                  required: {
-                     value: true,
-                     message: "Item is required",
-                  },
-               })}
-            >
-               <option value=""></option>
-               {itemList?.map((item) => {
-                  return (
-                  <option key={item?.id} value={item?.id?.toString()}>
-                     {item?.name}
-                  </option>
-                  );
-               })}
-            </select>
+            <div className="flex gap-x-3">
+               <select
+                  className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 disabled:cursor-not-allowed transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent placeholder:opacity-0 focus:placeholder:opacity-100 text-sm px-3 py-3 rounded-md border-blue-gray-200 focus:border-gray-900 !border-t-blue-gray-200 focus:!border-t-gray-900"
+                  {...register("item_id", {
+                     required: {
+                        value: true,
+                        message: "Item is required",
+                     },
+                  })}
+               >
+                  <option value=""></option>
+                  {itemList?.map((item) => {
+                     return (
+                     <option key={item?.id} value={item?.id?.toString()}>
+                        {item?.name}
+                     </option>
+                     );
+                  })}
+                  </select>
+                  <Button onClick={handleAddItem}>+</Button>
+            </div>
             <Typography variant="h6" color="blue-gray" className="-mb-3 flex justify-between">
-                  Amount
+                  Amount *
                   <p className="block font-semibold mb-[2px] text-red-500">
-                  {errors?.amount?.message}
-               </p>
+                     {errors?.amount?.message}
+                  </p>
             </Typography>
             <Input
                {...register("amount", {
@@ -203,6 +315,7 @@ export const AddDebt = () => {
             {isSubmitting ? <Spinner /> : "REGISTER"}
          </Button>
          </form>
+         {addItemModel}
       </Card>
    );
 };
